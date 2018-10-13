4a7a1da68d6a68c4741cd9f09a2555218dd80c6c
bigdan4712 <djday86@gmail.com>
2018-10-13 08:15:46 -0400

    Added additional features to script, added more user interaction for creation

--- create.sh
+++ create.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 read -p "VM Name: " VM
-read -p "Path to ISO: " OS_ISO
+read -p "ISO file: " OS_ISO
 read -p "Memory(In MB): " MEMORY
+read -p "HDD size(In MB): " DISK_SIZE
+read -p "VRAM size(In MB): " VRAM_SIZE
 
-#VM='CentOS-Server'
-#OS_ISO='/home/daniel/ISOs/centOS.iso'
-#MEMORY=4096
 
-vboxmanage createhd --filename $VM.vdi --size 32768
+vboxmanage createhd --filename $VM.vdi --size $DISK_SIZE
 vboxmanage createvm --name $VM --ostype "Linux_64" --register
 vboxmanage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
 vboxmanage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM.vdi
 vboxmanage storagectl $VM --name "IDE Controller" --add ide
-vboxmanage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium $OS_ISO
+vboxmanage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium /home/daniel/ISOs/$OS_ISO
 vboxmanage modifyvm $VM --ioapic on 
 vboxmanage modifyvm $VM --boot1 dvd --boot2 disk --boot3 none --boot4 none
-vboxmanage modifyvm $VM --memory $MEMORY --vram 16
+vboxmanage modifyvm $VM --memory $MEMORY --vram $VRAM_SIZE
 vboxmanage modifyvm $VM --nic1 bridged --bridgeadapter1 eno2
 
 
